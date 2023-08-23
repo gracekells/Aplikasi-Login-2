3d9275ce13f1b3c508b9b2dec3818713cc44d4de
Grace Kelly <113868919+gracekells@users.noreply.github.com>
2023-08-23 23:42:09 +0700

    Update login-script.js

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -1,22 +1,20 @@
 /** @module Login-Script */
-/*
- * Catatan:
- * Gunakan fungsi goToHome() untuk menampilkan halaman home
- * Gunakan fungsi goToLogin() untuk menampilkan halaman login
- * Gunakan fungsi showPopUp() untuk menampilkan pop up error
- */
-
-// membuat variabel loginFormElemnt untuk tampilan form
-// @constant {HTMLElement}
+/** 
+* Membuat variabel loginFormElement untuk tampilan form.
+* @constant {HTMLElement}
+*/
 const loginFormElement = document.querySelector('#loginForm');
 
-// membuat variabel inputEmailElement untuk tampilan input email.
-// @constant {HTMLElement}
-
+/** 
+* Membuat variabel inputEmailElement untuk tampilan input email.
+* @constant {HTMLElement}
+*/
 const inputEmailElement = document.querySelector('#inputEmail');
 
-// Membuat variabel inputPasswordElement untuk tampilan input password.
-// @constant {HTMLElement}
+/** 
+* Membuat variabel inputPasswordElement untuk tampilan input password.
+* @constant {HTMLElement}
+*/
 const inputPasswordElement = document.querySelector('#inputPassword');
 
 /** 
